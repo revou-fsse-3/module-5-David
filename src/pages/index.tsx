ae01ad4a32fcc4cfccb8e3c308e7408452ad3e11
davidwds <davidwahyuds@gmail.com>
2024-02-01 04:55:57 +0700

    fixing link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/Navbar";
 import PokeInfo from "@/components/PokemonInfo";
-import SearchPokemon from "@/components/SearchPokemonIndex";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <SearchPokemon />
       <PokeInfo />
     </>
   );
